ae799e67a9d6befec46001d405e7618fcafc59a5
Harel Karni <MountainGodRays@gmail.com>
2022-12-15 12:17:30 +0200

    added toggle form in contact us page

--- js/contact.js
+++ js/contact.js
@@ -3,3 +3,18 @@ function sendMessage() {
         + document.getElementById("email").value
         + " sent\:&?body=" + document.getElementById("message").value;
 }
+
+function showForm(formWrapperID) {
+    let form = document.getElementById(formWrapperID);
+    form.style.display = "block"
+}
+
+function hideAllForms() {
+    let wrapperIDs = ["emailFormWrapper", "SMSFormWrapper", "faxFormWrapper"];
+    for (let wrapper of wrapperIDs) {
+        document.getElementById(wrapper).style.display = "none";
+    }
+}
+
+hideAllForms();
+showForm("emailFormWrapper");
